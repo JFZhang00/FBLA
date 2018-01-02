1e65af7325df63f7f92ff23220d9b1ec9373b686
Jason Zhang <jasonzhanger@gmail.com>
2018-01-01 23:47:43 -0700

    FBLA Game: - Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        if (moveHorizontal > 0.5f || moveHorizontal < -0.5f){
+            transform.Translate(new Vector3(moveHorizontal * moveSpeed * Time.deltaTime, 0f, 0f));
+        } 
+        if (moveVertical > 0.5f || moveVertical < -0.5f){
+            transform.Translate(new Vector3(0f, moveVertical * moveSpeed * Time.deltaTime, 0f));
+        } 
+
+
+	}
+}
